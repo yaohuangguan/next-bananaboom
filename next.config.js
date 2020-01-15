b7e5c615719dc232d0e74a5ba738857dde754cf2
yaohuangguan <yaob@miamioh.edu>
2020-01-15 17:07:25 +0800

    s

--- next.config.js
+++ next.config.js
@@ -1,23 +1,48 @@
 const withSass = require("@zeit/next-sass");
 const withCSS = require("@zeit/next-css");
 const withOffline = require("next-offline");
-const nextConfig = withCSS(
-  withSass({
-    webpack(config, options) {
-      config.module.rules.push({
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 100000
+const nextConfig = withOffline(
+  withCSS(
+    withSass({
+      webpack(config, options) {
+        config.module.rules.push({
+          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+          use: {
+            loader: "url-loader",
+            options: {
+              limit: 100000
+            }
           }
-        }
-      });
-      return config;
-    }
-  })
+        });
+        return config;
+      }
+    })
+  )
 );
-const offlineConfig = {
+// const offline = {
+//   target: "serverless",
+//   workboxOpts: {
+//     swDest: "static/service-worker.js",
+//     runtimeCaching: [
+//       {
+//         urlPattern: /^https?.*/,
+//         handler: "NetworkFirst",
+//         options: {
+//           cacheName: "https-calls",
+//           networkTimeoutSeconds: 15,
+//           expiration: {
+//             maxEntries: 150,
+//             maxAgeSeconds: 30 * 24 * 60 * 60
+//           },
+//           cacheableResponse: {
+//             statuses: [0, 200]
+//           }
+//         }
+//       }
+//     ]
+//   }
+// };
+module.exports = {
   target: "serverless",
   workboxOpts: {
     swDest: "static/service-worker.js",
@@ -38,6 +63,6 @@ const offlineConfig = {
         }
       }
     ]
-  }
+  },
+  ...nextConfig
 };
-module.exports = withOffline({ ...offlineConfig, ...nextConfig });
