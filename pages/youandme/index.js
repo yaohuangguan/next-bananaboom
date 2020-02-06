e1d6fb01075030f2ebfbd9c1e1b4b7009dd93696
yaohuangguan <yaob@miamioh.edu>
2020-02-06 10:53:47 +0800

    s

--- pages/youandme/index.js
+++ pages/youandme/index.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import {useRouter} from 'next/router'
+const index = ({currentUser}) => {
+  const router = useRouter();
 
-const index = () => {
+  useEffect(() => {
+    const checkLogin = () => {
+      if(currentUser){
+        if(currentUser.vip){
+          return null
+        } 
+      }
+     return router.back();
+    };
+    checkLogin();
+    return () => {};
+  },[currentUser]);
+  const getVip = () => currentUser ? currentUser.vip : null
   return (
     <div>
-      只有我们知道啊，宝贝 <br/>
-      这是咱俩的秘密基地
+    {getVip() ? (<div className='row'>
+      <div className="content col-md-6 p-3 m-3">
+      <h1>Sam & Cennifer</h1>
+        只有我们知道啊，宝贝 <br/>
+        这是咱俩的秘密基地
+      </div>
+
+      <div className='input'>
+        <input type="text"/> 
+          <input type="submit" value='❤' />
+      </div>
+    </div>):null}
     </div>
+     
+
   )
 }
 
