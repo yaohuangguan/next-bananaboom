db5a640801525ecaec27768616c4125f2ca0938d
yaohuangguan <yaob@miamioh.edu>
2020-02-27 19:28:16 +0800

    d

--- next.config.js
+++ next.config.js
@@ -1,57 +1,29 @@
 const withSass = require("@zeit/next-sass");
 const withCSS = require("@zeit/next-css");
-const withOffline = require("next-offline");
 
-
-const nextConfig = withOffline(
-  withCSS(
-    withSass({
-      webpack(config, options) {
-        config.module.rules.push(
-          {
-            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-            use: {
-              loader: "url-loader",
-              options: {
-                limit: 100000
-              }
-            }
-          },
-          {
-            test: /\.(ogg|mp3|wav|mpe?g)$/i,
-            loader: 'file-loader',
+const nextConfig = withCSS(
+  withSass({
+    webpack(config, options) {
+      config.module.rules.push(
+        {
+          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+          use: {
+            loader: "url-loader",
             options: {
-              name: '[path][name].[ext]'
+              limit: 100000
             }
           }
-        );
-        return config;
-      }
-    })
-  )
+        },
+        {
+          test: /\.(ogg|mp3|wav|mpe?g)$/i,
+          loader: "file-loader",
+          options: {
+            name: "[path][name].[ext]"
+          }
+        }
+      );
+      return config;
+    }
+  })
 );
-// const offline = {
-//   target: "serverless",
-//   workboxOpts: {
-//     swDest: "static/service-worker.js",
-//     runtimeCaching: [
-//       {
-//         urlPattern: /^https?.*/,
-//         handler: "NetworkFirst",
-//         options: {
-//           cacheName: "https-calls",
-//           networkTimeoutSeconds: 15,
-//           expiration: {
-//             maxEntries: 150,
-//             maxAgeSeconds: 30 * 24 * 60 * 60
-//           },
-//           cacheableResponse: {
-//             statuses: [0, 200]
-//           }
-//         }
-//       }
-//     ]
-//   }
-// };
-
 module.exports = { ...nextConfig, target: "serverless" };
