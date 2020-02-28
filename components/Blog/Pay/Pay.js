232aa634e625cce09e5ba2862393c71269134528
yaohuangguan <yaob@miamioh.edu>
2020-02-28 15:01:00 +0800

    d

--- components/Blog/Pay/Pay.js
+++ components/Blog/Pay/Pay.js
@@ -2,37 +2,42 @@ import React, { useRef } from "react";
 
 const Pay = () => {
   const pay = useRef(null);
+  const dashang = useRef(null)
   const handleChange = () => {
     pay.current.style.visibility = "visible";
+    dashang.current.style.visibility = "hidden";
   };
   return (
-    <div>
+    <>
       <div
         style={{
           display: "flex",
+          flexDirection:'column',
           justifyContent: "center",
+          alignItems:'center',
           position: "relative"
         }}
         onClick={handleChange}
       >
-        <img
-          src="https://res.cloudinary.com/next-bananaboom/image/upload/v1582866923/dashang_r3ltt4.jpg"
-          width="10%"
-          alt="da shang"
-          style={{}}
-        />
-
+        <div ref={dashang} style={{textAlign:'center'}}>
+          <p>Thank you for your support!!</p>
           <img
-            src="/pay.jpeg"
-            alt="wechat pay"
-            width="20%"
-            style={{ position: "absolute", visibility: "hidden" }}
-            ref={pay}
+            src="https://res.cloudinary.com/next-bananaboom/image/upload/v1582866923/dashang_r3ltt4.jpg"
+            width="40%"
+            alt="da shang"
           />
+        </div>
 
-
+        <img
+          src="/pay.jpeg"
+          alt="wechat pay"
+          width="30%"
+          style={{ position: "absolute", visibility: "hidden" }}
+          ref={pay}
+        />
+       
       </div>
-    </div>
+    </>
   );
 };
 
