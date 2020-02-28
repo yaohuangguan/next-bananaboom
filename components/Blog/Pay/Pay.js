28198df20630f3b5e26316287622c0c3ccb33a49
yaohuangguan <yaob@miamioh.edu>
2020-02-28 15:06:33 +0800

    d

--- components/Blog/Pay/Pay.js
+++ components/Blog/Pay/Pay.js
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 
 const Pay = () => {
   const pay = useRef(null);
-  const dashang = useRef(null)
+  const dashang = useRef(null);
   const handleChange = () => {
     pay.current.style.visibility = "visible";
     dashang.current.style.visibility = "hidden";
@@ -12,30 +12,32 @@ const Pay = () => {
       <div
         style={{
           display: "flex",
-          flexDirection:'column',
+          flexDirection: "column",
           justifyContent: "center",
-          alignItems:'center',
+          alignItems: "center",
           position: "relative"
         }}
         onClick={handleChange}
       >
-        <div ref={dashang} style={{textAlign:'center'}}>
+        <div ref={dashang} style={{ textAlign: "center" }}>
           <p>Thank you for your support!!</p>
           <img
             src="https://res.cloudinary.com/next-bananaboom/image/upload/v1582866923/dashang_r3ltt4.jpg"
-            width="40%"
+            width="20%"
             alt="da shang"
           />
         </div>
 
-        <img
-          src="/pay.jpeg"
-          alt="wechat pay"
-          width="30%"
-          style={{ position: "absolute", visibility: "hidden" }}
+        <div
           ref={pay}
-        />
-       
+          style={{
+            textAlign: "center",
+            position: "absolute",
+            visibility: "hidden"
+          }}
+        >
+          <img src="/pay.jpeg" alt="wechat pay" width="35%" />
+        </div>
       </div>
     </>
   );
