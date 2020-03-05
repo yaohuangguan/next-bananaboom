6e7db95bc48f9919f744d7cb1a26c9067f0f40e8
yaohuangguan <yaob@miamioh.edu>
2020-03-05 18:19:46 +0800

    d

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const next = require('next')
-const express = require('express')
-const compression = require('compression')
-const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+// const next = require('next')
+// const express = require('express')
+// const compression = require('compression')
+// const port = parseInt(process.env.PORT, 10) || 3000;
+// const dev = process.env.NODE_ENV !== "production";
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
 
-app
-  .prepare()
-  .then(() => {
-    const server = express();
-    server.use(compression());
-    process.setMaxListeners(0);
-    // server.get("/service-worker.js", serviceWorker(app));
-    server.all("*", (req, res) => {
-      return handle(req, res);
-    });
+// app
+//   .prepare()
+//   .then(() => {
+//     const server = express();
+//     server.use(compression());
+//     process.setMaxListeners(0);
+//     // server.get("/service-worker.js", serviceWorker(app));
+//     server.all("*", (req, res) => {
+//       return handle(req, res);
+//     });
 
-    server.listen(port, (err) => {
-      if (err) throw err;
-      console.log(`> Ready on ${port}`);
-    });
-  })
-  .catch((ex) => {
-    console.error(ex.stack);
-    process.exit(1);
-  });
+//     server.listen(port, (err) => {
+//       if (err) throw err;
+//       console.log(`> Ready on ${port}`);
+//     });
+//   })
+//   .catch((ex) => {
+//     console.error(ex.stack);
+//     process.exit(1);
+//   });
