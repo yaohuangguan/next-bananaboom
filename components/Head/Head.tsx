2c066622718203be3c51282daaf7f983702ca83d
yaohuangguan <yaob@miamioh.edu>
2020-03-05 14:04:00 +0800

    d

--- components/Head/Head.tsx
+++ components/Head/Head.tsx
@@ -4,7 +4,7 @@ const HeadConfig = () => {
   return (
     <Head>
       <meta charSet="utf-8" />
-      <title>Moviegoer</title>
+      <title>Moviegoer || 姚柏杨</title>
       <link rel="icon" href="/favicon.ico" />
 
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
