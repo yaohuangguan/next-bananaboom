70d14d66b938d5ed6db8e1e82acff40a39112ab0
yaohuangguan <yaob@miamioh.edu>
2020-03-30 17:07:39 +0800

    regular-push

--- components/Header/DropDown/DropDown.tsx
+++ components/Header/DropDown/DropDown.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import api from "../../../utils/Api";
 import firebase from "../../../firebase/firebase";
 import "./DropDown.scss";
 const DropDown = ({ currentUser }) => {
-  const router = useRouter()
+  const router = useRouter();
   const logout = async () => {
     if (typeof window != "undefined") {
       localStorage.removeItem("token");
@@ -20,8 +20,16 @@ const DropDown = ({ currentUser }) => {
     return router.pathname === "/" ? "Dashboard" : "个人中心";
   };
   const { uid, displayName, photoURL, _id } = currentUser;
-  const getGreeting = () =>
-    router.pathname == "/" ? `Hi,${displayName}.` : `你好,${displayName}`;
+  const getGreeting = () => {
+    const hour = new Date().getHours();
+    const greeting =
+      hour < 12 ? "Good Morning" : hour < 18 ? "Good Afternoon" : "Good Evening";
+    const greeting_cn = hour < 12 ? "早上好" : hour < 18 ? "下午好" : "晚上好";
+    return router.pathname == "/"
+      ? `${greeting},${displayName}.`
+      : `${greeting_cn},${displayName}`;
+  };
+
   const googleLogOut = () => {
     firebase.auth.signOut();
     router.reload();
